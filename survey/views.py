71d804a1108987f24c1beb141c471f1f039def8d
Suntae Kim <kst6294@gmail.com>
2021-08-05 21:37:18 +0900

    [Modify] survey view mix error

--- survey/views.py
+++ survey/views.py
@@ -1,10 +1,12 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.views    import APIView
 
 from survey.serializers import SurveySerializer
-from survey.models import Survey
+from survey.models      import Survey
+
 
 class ListView(APIView):
     def get(self, request):
-        data = SurveySerializer(Survey.objects.get(id=1)).data
-        return Response(data)
+        survey = Survey.objects.all()
+        serializer = SurveySerializer(survey, many=True)
+        return Response(serializer.data)
